ef9254630854003f32c11666e0d46306c42a3f47
alerciosilva9 <silvaalercio@gmail.com>
2023-05-12 00:07:05 -0300

    Criando projeto e implementando ORM

---
+++ src/main/java/com/dsgames/entities/Game.java
@@ -0,0 +1,151 @@
+package com.dsgames.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_game")
+public class Game {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String teste;
+	@Column(name="game_year")
+	private Integer year;
+	private Double Score;
+	
+	private String genre;
+	private String platforms;
+	private String img_url;
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+	
+	public Game() {
+		
+	}
+
+	public Game(Long id, String title, String teste, Integer year, Double score, String genre, String plataform,
+			String imageUrl, String shortDescription, String longDescription) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.teste = teste;
+		this.year = year;
+		Score = score;
+		this.genre = genre;
+		this.platforms = plataform;
+		this.img_url = imageUrl;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTeste() {
+		return teste;
+	}
+
+	public void setTeste(String teste) {
+		this.teste = teste;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public Double getScore() {
+		return Score;
+	}
+
+	public void setScore(Double score) {
+		Score = score;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlataform() {
+		return platforms;
+	}
+
+	public void setPlataform(String plataform) {
+		this.platforms = plataform;
+	}
+
+	public String getImageUrl() {
+		return img_url;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.img_url = imageUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
